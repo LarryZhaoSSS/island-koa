6b546b2482ad800e0c5e0dc5609fc7d0d5072719
LarryKameZhao <larryzhao1994@outlook.com>
2019-08-05 07:31:23 +0800

    initial model

---
+++ app/models/user.js
@@ -0,0 +1,20 @@
+const {db} = require('../../core/db')
+const {Sequelize, Model} = require('sequelize')
+
+class User extends Model {
+
+}
+User.init({
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nickname: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+  openid:{
+    type:Sequelize.STRING(64),
+    unique: true
+  }
+})
---
+++ core/db.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize')
+const {
+  dbName,
+  host,
+  port,
+  user,
+  password
+} = require('../config/dbConfig').database
+const sequelize = new Sequelize(dbName,user,password,{
+  dialect:'mysql',
+  host,
+  port,
+  logging: true,
+  timezone: '+08:00',
+  define:{
+
+  }
+})
+
+module.exports = {
+  db:sequelize
+}
