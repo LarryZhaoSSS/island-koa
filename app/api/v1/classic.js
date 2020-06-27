269ad9e996e224bb437383f7c6124ff9ca88f3bc
LarryKameZhao <larryzhao1994@outlook.com>
2020-06-27 19:46:33 +0800

    mina  test

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,11 +1,17 @@
 const Router = require('koa-router');
+const { Flow } = require('../../models/flow');
 const router = new Router({
   prefix: '/v1/classic',
 });
 const { Auth } = require('../../../middlewares/auth');
 const { PositiveIntegerValidator } = require('../../validators/validator');
 
-router.get('/latest', new Auth(8).m, async (ctx, next) => {
-  ctx.body = ctx.auth.uid;
+router.get('/latest', new Auth().m, async (ctx, next) => {
+  const flow = await Flow.findOne({
+    order: [['index', 'DESC']],
+  });
+  console.log('------latest----');
+  console.log(flow);
+  ctx.body = flow;
 });
 module.exports = router;
