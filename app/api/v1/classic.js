901ca0defb937149b6d7db05345690b0ceed8ff5
LarryKameZhao <larryzhao1994@outlook.com>
2020-06-29 08:11:25 +0800

    latest api

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router');
 const { Flow } = require('../../models/flow');
+const { Art } = require('../../models/art');
 const router = new Router({
   prefix: '/v1/classic',
 });
@@ -10,8 +11,8 @@ router.get('/latest', new Auth().m, async (ctx, next) => {
   const flow = await Flow.findOne({
     order: [['index', 'DESC']],
   });
-  console.log('------latest----');
-  console.log(flow);
-  ctx.body = flow;
+  const art = await Art.getData(flow.art_id, flow.type);
+  art.setDataValue('index', flow.index);
+  ctx.body = art;
 });
 module.exports = router;
